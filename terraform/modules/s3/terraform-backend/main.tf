3b01249014c4387426a876034ef15aa4dc3d1c4c
tqer39 <tqer39@gmail.com>
2022-05-04 08:42:07 +0900

    流用するリソースのプロパティをidではなくbucketへ変更

--- terraform/modules/s3/terraform-backend/main.tf
+++ terraform/modules/s3/terraform-backend/main.tf
@@ -3,12 +3,12 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.this.bucket
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.this.bucket
 
   versioning_configuration {
     status = "Enabled"
