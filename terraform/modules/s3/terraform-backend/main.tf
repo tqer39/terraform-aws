71681bff9e0b346686f379f2a675915b30c80c1e
tqer39 <tqer39@gmail.com>
2022-05-04 08:49:13 +0900

    public access blockの設定を追加

--- terraform/modules/s3/terraform-backend/main.tf
+++ terraform/modules/s3/terraform-backend/main.tf
@@ -14,3 +14,12 @@ resource "aws_s3_bucket_versioning" "this" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
