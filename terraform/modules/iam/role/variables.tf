9df187adee65f308c8976a737f9b7a26a781bcb8
tqer39 <tqer39@gmail.com>
2022-06-04 17:47:34 +0900

    aws_iam_roleの汎用moduleを追加

---
+++ terraform/modules/iam/role/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_iam_role" {
+  description = "Provides an IAM role. aws_iam_role, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role"
+  type        = any
+  default     = null
+}
