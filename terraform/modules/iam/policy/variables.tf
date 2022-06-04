209abffcb741c361432e57dea570cdffb4beed8b
tqer39 <tqer39@gmail.com>
2022-06-04 17:47:16 +0900

    aws_iam_policyの汎用moduleを追加

---
+++ terraform/modules/iam/policy/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_iam_policy" {
+  description = "Provides an IAM policy. aws_iam_policy, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy"
+  type        = any
+  default     = null
+}
