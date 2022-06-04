6271851b724768716dfd3917e850ef4f85a25541
tqer39 <tqer39@gmail.com>
2022-06-04 17:34:24 +0900

    aws_cloudfront_origin_access_identityの汎用moduleを追加

---
+++ terraform/modules/cloudfront/origin_access_identity/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_cloudfront_origin_access_identity" {
+  description = "Creates an Amazon CloudFront origin access identity. aws_cloudfront_origin_access_identity, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_identity"
+  type        = any
+  default     = null
+}
