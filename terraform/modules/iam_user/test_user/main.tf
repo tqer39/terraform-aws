3323c5989dcb52dbfb5cd53604f4d5118b7e8dcf
tqer39 <tqer39@gmail.com>
2022-06-05 11:47:37 +0900

    設計上不要になったソースを削除

--- terraform/modules/iam_user/test_user/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user" "this" {
-  name = var.user_name
-  path = "/"
-}
--- terraform/modules/iam_user/test_user/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "user_name" {
-  type = string
-}
-
-# variable "user_names" {
-#   type = tuple([string, string, string])
-# }
