64e1219c9dd61af8f0c0b877f00e34fd627e39c0
tqer39 <tqer39@gmail.com>
2022-06-05 18:02:36 +0900

    aclは使わないのが推奨

--- terraform/usecases/base/terraform_tfstate_backend/main.tf
+++ terraform/usecases/base/terraform_tfstate_backend/main.tf
@@ -4,9 +4,6 @@ module "s3-bucket" {
   aws_s3_bucket = {
     bucket = var.bucket_name
   }
-  aws_s3_bucket_acl = {
-    acl = "private"
-  }
   aws_s3_bucket_versioning = {
     versioning_configuration = [
       {
