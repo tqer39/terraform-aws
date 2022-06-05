4c5e62eb0999c92b7e342d537334a7c2326cf830
tqer39 <tqer39@gmail.com>
2022-06-05 17:14:11 +0900

    aws_iam_userの汎用moduleを追加

---
+++ terraform/modules/iam/user/main.tf
@@ -0,0 +1,6 @@
+resource "aws_iam_user" "this" {
+  name                 = lookup(var.aws_iam_role, "name", "")
+  force_destroy        = lookup(var.aws_iam_role, "force_destroy", false)
+  path                 = lookup(var.aws_iam_role, "path", "")
+  permissions_boundary = lookup(var.aws_iam_role, "permissions_boundary", "")
+}
---
+++ terraform/modules/iam/user/outputs.tf
@@ -0,0 +1,7 @@
+output "name" {
+  value = aws_iam_user.this.name
+}
+
+output "arn" {
+  value = aws_iam_user.this.arn
+}
