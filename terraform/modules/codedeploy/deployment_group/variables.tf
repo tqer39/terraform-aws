12b8b2eeb3d21cbfbf0fea20097c5ceda0146ea2
tqer39 <tqer39@gmail.com>
2022-06-11 11:28:37 +0900

    aws_codedeploy_deployment_groupの汎用moduleを追加

---
+++ terraform/modules/codedeploy/deployment_group/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_codedeploy_deployment_group" {
+  description = "Provides a CodeDeploy Deployment Group for a CodeDeploy Application. aws_codedeploy_deployment_group, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codedeploy_deployment_group"
+  type        = any
+  default     = null
+}
