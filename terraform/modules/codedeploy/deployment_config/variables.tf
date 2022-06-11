c031dabb7d3d9f34d9fc2125f77f34f80dd2a916
tqer39 <tqer39@gmail.com>
2022-06-11 11:26:01 +0900

    aws_codedeploy_deployment_configの汎用moduleを追加

---
+++ terraform/modules/codedeploy/deployment_config/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_codedeploy_deployment_config" {
+  description = "Provides a CodeDeploy deployment config for an application aws_codedeploy_deployment_config, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codedeploy_deployment_config"
+  type        = any
+  default     = null
+}
