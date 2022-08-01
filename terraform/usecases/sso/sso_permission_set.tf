9b7d7ff6a4deba465c9716415fbf4019b7f4820c
tqer39 <tqer39@gmail.com>
2022-08-01 18:19:08 +0900

    詳細を追加

--- terraform/usecases/sso/sso_permission_set.tf
+++ terraform/usecases/sso/sso_permission_set.tf
@@ -5,6 +5,7 @@ resource "aws_ssoadmin_permission_set" "main" {
   ])
 
   name             = each.value
+  description      = "Managed by Terraform"
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
   session_duration = "PT12H"
 }
