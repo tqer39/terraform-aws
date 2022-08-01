6b1a97b57eba69eb96e50838fc449973af9e4998
tqer39 <tqer39@gmail.com>
2022-08-01 17:32:23 +0900

    outputを追加

--- terraform/usecases/sso/outputs.tf
+++ terraform/usecases/sso/outputs.tf
@@ -1,3 +1,14 @@
 output "sso_instance_arns" {
-  value = data.aws_ssoadmin_instances.this.arns
+  description = "Set of Amazon Resource Names (ARNs) of the SSO Instances."
+  value       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+}
+
+output "sso_instance_id" {
+  description = "AWS Region."
+  value       = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+}
+
+output "sso_instance_identity_store_ids" {
+  description = "Set of identifiers of the identity stores connected to the SSO Instances."
+  value       = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)
 }
