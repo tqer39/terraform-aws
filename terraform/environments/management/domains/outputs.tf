01d672830dbda2bdbfc8ff0f1660497b39430a64
tqer39 <tqer39@gmail.com>
2022-08-07 06:11:59 +0900

    outputの指定ミス

--- terraform/environments/management/domains/outputs.tf
+++ terraform/environments/management/domains/outputs.tf
@@ -1,7 +1,7 @@
 output "name_servers" {
-  value = aws_route53_zone.this.name_servers
+  value = module.root_domain.name_servers
 }
 
 output "zone_id" {
-  value = aws_route53_zone.this.zone_id
+  value = module.root_domain.zone_id
 }
