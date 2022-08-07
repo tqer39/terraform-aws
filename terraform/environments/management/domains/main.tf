9232518355a99ee28cab1e974a6440b99a47d995
tqer39 <tqer39@gmail.com>
2022-08-07 17:20:26 +0900

    別のAWSアカウントのremote_tfstateを取得できるか実験

--- terraform/environments/management/domains/main.tf
+++ terraform/environments/management/domains/main.tf
@@ -3,3 +3,17 @@ module "root_domain" {
 
   domain_name = "tqer39.com"
 }
+
+# locals {
+#   aws_account_id = {
+#     "portfolio" = "072693953877"
+#   }
+# }
+
+resource "aws_route53_record" "portfolio" {
+  zone_id = module.root_domain.zone_id
+  name    = "portfolio.tqer39.com"
+  type    = "NS"
+  ttl     = "30"
+  records = data.terraform_remote_state.portfolio-domains.outputs.name_servers
+}
