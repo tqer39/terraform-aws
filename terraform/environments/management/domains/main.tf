138b9eeea94071c8e2917ba33a004c222b62799a
tqer39 <tqer39@gmail.com>
2022-08-07 22:51:53 +0900

    NSレコードをハードコーディング

--- terraform/environments/management/domains/main.tf
+++ terraform/environments/management/domains/main.tf
@@ -4,16 +4,17 @@ module "root_domain" {
   domain_name = "tqer39.com"
 }
 
-# locals {
-#   aws_account_id = {
-#     "portfolio" = "072693953877"
-#   }
-# }
-
 resource "aws_route53_record" "portfolio" {
   zone_id = module.root_domain.zone_id
   name    = "portfolio.tqer39.com"
   type    = "NS"
   ttl     = "30"
-  records = data.terraform_remote_state.portfolio-domains.outputs.name_servers
+  # TODO: portfolio のアカウントから取得できるようになったらハードコーディングをやめる
+  # records = data.terraform_remote_state.portfolio-domains.outputs.name_servers
+  records = [
+    "ns-1761.awsdns-28.co.uk.",
+    "ns-958.awsdns-55.net.",
+    "ns-500.awsdns-62.com.",
+    "ns-1366.awsdns-42.org."
+  ]
 }
