b5af45d2660f4fd8231e3c8174ef916c4db618ee
tqer39 <tqer39@gmail.com>
2022-08-27 07:57:03 +0900

    Revert "引数を修正"
    This reverts commit 79782d4f736083f4d032d16bdd8826d18858c4d8.

--- terraform/github/environments/blog/main.tf
+++ terraform/github/environments/blog/main.tf
@@ -1,9 +1,7 @@
 module "blog" {
   source = "../../modules/repository"
 
-  github_repository = {
-    name        = "blog"
-    description = "blog"
-    visibility  = "public"
-  }
+  name        = "blog"
+  description = "blog"
+  visibility  = "public"
 }
