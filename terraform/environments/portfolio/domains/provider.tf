c28034da1cf5a4a1e532ed25d475892bdc80fb8e
tqer39 <tqer39@gmail.com>
2023-03-19 10:43:09 +0000

    refactor

--- terraform/environments/portfolio/domains/provider.tf
+++ terraform/environments/portfolio/domains/provider.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region = local.region.apne1
-
   default_tags {
     tags = merge(local.common_tags, {
-      "${local.prefix}:source_path" = "https://github.com/tqer39/private-lab/tree/main/terraform/environments/${local.env_name}/domains"
+      "${local.prefix}:source_path" = "./terraform/environments/${local.aws_env_name}/domains"
     })
   }
 }
--- terraform/environments/portfolio/domains/terraform.tf
+++ terraform/environments/portfolio/domains/terraform.tf
@@ -1,17 +1,15 @@
 terraform {
-  required_version = "=1.2.8"
-
+  required_version = "1.4.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.58.0"
     }
   }
-
   backend "s3" {
     bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
     encrypt = true
-    key     = "private-lab/terraform/environments/portfolio/portfolio-domains.tfstate"
+    key     = "terraform-aws/terraform/environments/portfolio/portfolio-domains.tfstate"
     region  = "ap-northeast-1"
   }
 }
