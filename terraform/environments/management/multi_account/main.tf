c72f048f2ada5007b5c8e770f6e50664098eb3a7
tqer39 <tqer39@gmail.com>
2023-03-19 08:00:38 +0000

    不要なコメントを削除

--- terraform/environments/management/multi_account/main.tf
+++ terraform/environments/management/multi_account/main.tf
@@ -26,12 +26,6 @@ locals {
       id          = "sandbox",
       parent_type = "Sandbox"
     },
-    # {
-    #   name              = "Portfolio - dev",
-    #   id                = "portfolio-dev",
-    #   parent_type       = "Security",
-    #   close_on_deletion = true
-    # },
     {
       name        = "Portfolio",
       id          = "portfolio",
