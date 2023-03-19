48f96564c59e32f7d6bdaa636ade2c89451b8c52
tqer39 <tqer39@gmail.com>
2023-03-19 08:48:41 +0000

    portfolioのリソースをリファクタリング

--- terraform/environments/portfolio/shared/locals.tf
+++ terraform/environments/portfolio/shared/locals.tf
@@ -1,6 +1,6 @@
 locals {
   aws_account_id = "072693953877"
-  env_name       = "portfolio"
+  aws_env_name   = "portfolio"
   organization   = "tqer39"
   platform       = "aws"
   prefix         = "pl"
@@ -13,9 +13,9 @@ locals {
 
 locals {
   common_tags = {
-    "${local.prefix}:environment" = local.env_name
+    "${local.prefix}:environment" = local.aws_env_name
     "${local.prefix}:iac"         = "terraform"
-    "${local.prefix}:owner"       = "${local.organization}+${local.platform}-${local.env_name}@gmail.com"
+    "${local.prefix}:owner"       = "${local.organization}+${local.platform}-${local.aws_env_name}@gmail.com"
     "${local.prefix}:product"     = "private-lab"
     "${local.prefix}:repository"  = "${local.organization}/${local.repository}"
   }
