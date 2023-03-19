b2369b0f312f3538bb48c998416bcf35a6fe522a
tqer39 <tqer39@gmail.com>
2023-03-19 07:24:44 +0000

    ssoのリソースをリファクタリング

--- terraform/environments/management/single_sign_on/provider.tf
+++ terraform/environments/management/single_sign_on/provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
   default_tags {
     tags = merge(local.common_tags, {
-      "${local.prefix}:source_path" = "https://github.com/tqer39/private-lab/tree/main/terraform/environments/${local.env_name}/single_sign_on"
+      "${local.prefix}:source_path" = "./terraform/environments/${local.aws_env_name}/single_sign_on"
     })
   }
 }
