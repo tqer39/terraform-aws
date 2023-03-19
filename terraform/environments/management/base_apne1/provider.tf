d950f9cb2080d484d1a3d3f86494b88c4f537ba4
tqer39 <tqer39@gmail.com>
2023-03-19 02:12:00 +0000

    refactor

--- terraform/environments/management/base_apne1/provider.tf
+++ terraform/environments/management/base_apne1/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = local.region.apne1
-
   default_tags {
     tags = merge(local.common_tags, {
       "${local.prefix}:source_path" = "https://github.com/tqer39/private-lab/tree/main/terraform/environments/${local.env_name}/base_apne1"
