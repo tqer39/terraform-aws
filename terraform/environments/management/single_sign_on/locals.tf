33b0f7a015b9c0248fe516db3be616e5777c5500
tqer39 <tqer39@gmail.com>
2023-03-24 23:41:40 +0000

    AWSアカウント情報を棚卸し

--- terraform/environments/management/single_sign_on/locals.tf
+++ terraform/environments/management/single_sign_on/locals.tf
@@ -1,10 +1,10 @@
 # tflint-ignore: terraform_unused_declarations
 locals {
   aws_account_ids = {
-    "audit"         = "941697148827"
-    "log"           = "612023513349"
-    "management"    = "577523824419"
-    "portfolio-dev" = "373303485727"
-    "sandbox"       = "107662415716"
+    "audit"      = "941697148827"
+    "log"        = "612023513349"
+    "management" = "577523824419"
+    "portfolio"  = "072693953877"
+    "sandbox"    = "107662415716"
   }
 }
