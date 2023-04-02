c8f977b0bd99a5a7fa7feeec91f9b2dfa3ab2e3e
tqer39 <tqer39@gmail.com>
2023-04-02 01:42:39 +0000

    Change Organization

--- terraform/environments/sandbox/shared/locals.tf
+++ terraform/environments/sandbox/shared/locals.tf
@@ -1,7 +1,7 @@
 locals {
   aws_account_id = "107662415716"
   aws_env_name   = "sandbox"
-  organization   = "LifeTechEase"
+  organization   = "tqer39"
   platform       = "aws"
   prefix         = "pl"
   owner          = "tqer39"
