4ebfc6a324435e8dca815a4cde0a6b8c71398260
tqer39 <tqer39@gmail.com>
2023-04-02 05:54:22 +0000

    terraform_githubからのデプロイを受け付けるusecaseを追加

---
+++ terraform/usecases/deploy_role/terraform_github/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_account_id" {
+  type        = string
+  description = "(Required) AWS account ID"
+}
+
+variable "aws_env_name" {
+  type        = string
+  description = "(Required) AWS Environment name"
+}
+
+variable "organization" {
+  type        = string
+  description = "(Required) GitHub organization name"
+}
+
+variable "repository" {
+  type        = string
+  description = "(Required) GitHub repository name"
+}
