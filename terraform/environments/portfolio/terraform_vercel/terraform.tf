dd3f26b2a53180c05f26a5b76b0af8e9695efb97
tqer39 <tqer39@gmail.com>
2023-05-18 07:12:44 +0000

    🍱 Vercelでデプロイするリソースを追加

---
+++ terraform/environments/portfolio/terraform_vercel/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.4.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
+    encrypt = true
+    key     = "terraform-aws/terraform/environments/portfolio/portfolio-terraform_vercel.tfstate"
+    region  = "ap-northeast-1"
+  }
+}
