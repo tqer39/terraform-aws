38d384511dbc721c29fefd34ecc1bd0f0fb12f37
t.ooyama <tqer39@gmail.com>
2023-05-18 17:37:24 +0900

    Feature/add deploy role terraform vercel (#281)
    * 🔧 Add deploy pipeline. terraform-vercel
    
    * 🍱 Vercelでデプロイするときのモジュールを追加
    
    * 🍱 Vercelでデプロイするリソースを追加
    
    * 🐛 デプロイパイプラインを抽出するスクリプトにバグがあったので修正
    
    * 🐛 set-matrix action のバグ修正
    
    * 🐛 terraform plan action のバグ修正
    
    * ♻️ 差分チェックフラグで判定するように変更
    
    * ♻️ set-matrixの結果がemptyじゃなければ実行
    
    * ♻️ update concurrency
    
    * 🔥 不要なコメントは非表示ではなく削除に変更
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .github/scripts/check-for-changes-in-terraform-files.sh
+++ .github/scripts/check-for-changes-in-terraform-files.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -u
-
 # $1: デプロイパイプラインのパス
-# $2: プルリクエストのヘッドブランチ
+# $2: ベースブランチ
+# $3: プルリクエストのヘッドブランチ
 echo "\$1: $1"
 echo "\$2: $2"
+echo "\$3: $3"
 
-git fetch origin main
 git fetch origin "$2"
-git switch -C "$2"
-CHANGED_FILES="$(git diff --name-only origin/main "$2" | sort -u)"
+git fetch origin "$3"
+git switch -C "$3"
+CHANGED_FILES="$(git diff --name-only "origin/$2" "$3" | sort -u)"
+
 echo "----------------------------------"
 echo "CHANGED_FILES: "
 echo "${CHANGED_FILES}"
@@ -27,29 +29,26 @@ fi
 # 戻り値
 # 1: 処理対象外
 function is_changed () {
-  changed_file=$2
+  changed_file="$2"
   compare_path="$(echo "$2" | sed -e "s/\.\///g")" # "./" を削除
-
   echo "\$changed_file: ${changed_file}"
   echo "\$compare_path: ${compare_path}/*.*"
-
   if [[ $2 =~ ${compare_path}/.*\..*$ ]]; then
     echo "デプロイパイプラインで使用しているリソースが変更されたので処理対象です。"
     exit 0
   fi
-
   tf_files=$(find "${compare_path}" -type f -name "*.tf")
-
   for tf_file in $tf_files; do
     if [[ "${tf_file}" =~ .*(provider|terraform)\.tf$ ]]; then
-      echo "${tf_file}: provider.tf, terraform.tf は対象外。"
+      # echo "[debug] ${tf_file}: provider.tf, terraform.tf は対象外。"
       continue
     fi
 
-    module_paths="$(grep -oP 'source\s*=\s*"\K[^"]+' "${tf_file}")"
-
+    # module "..." { の次の行の source があればそのパスを列挙する
+    module_paths=$(awk '/module .+ {/{getline; if($1=="source") print $3}' "${tf_file}" | sed 's/"//g')
+    # echo "[debug] ${module_paths}: ${module_paths}"
     if [ -z "${module_paths}" ]; then
-      echo "${tf_file}: No modules."
+      # echo "[debug] ${tf_file}: No modules."
       continue
     fi
 
@@ -57,12 +56,10 @@ function is_changed () {
       cd "${compare_path}" || exit 2
       abs_source_path="$(realpath -e "${module_path}")"
       cd - > /dev/null || exit 2
-
-      # 再起処理で追跡
+      # 再帰処理で追跡
       is_changed "${changed_file}" "${abs_source_path}"
     done
   done
-
   return 1
 }
 
@@ -71,7 +68,6 @@ for file in $CHANGED_FILES; do
     echo "共通のワークフローが修正されたのでこのデプロイパイプラインは処理対象です。"
     exit 0
   fi
-
   is_changed "${file}" "$1"
 done
 
---
+++ terraform/usecases/deploy_role/terraform_vercel/main.tf
@@ -0,0 +1,58 @@
+/*
+ * # Usage
+ *
+ * - GitHub Actions から AWS へアクセスするときに使用する OIDC 認証の ID Provider にアクセスする IAM ロールを作成します。
+ * - [terraform-vercel](https://github.com/tqer39/terraform-vercel) 用です。
+ */
+
+data "aws_iam_policy" "deploy_deny_specifics" {
+  name = "deploy-deny-specifics"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+    principals {
+      type = "Federated"
+      identifiers = [
+        "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com",
+      ]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:${var.organization}/${var.repository}:*",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid = ""
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = [
+      "arn:aws:s3:::terraform-tfstate-tqer39-072693953877-ap-northeast-1/terraform-vercel/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name   = "${var.aws_env_name}-${var.repository}-deploy"
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.aws_env_name}-${var.repository}-deploy"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = [
+    aws_iam_policy.this.arn,
+    data.aws_iam_policy.deploy_deny_specifics.arn,
+  ]
+}
