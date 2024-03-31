42143efff0f34139037f95fb254aec6141dcc4b2
Takeru O'oyama <tqer39@gmail.com>
2024-03-31 22:55:03 +0900

    time-capsuleリポジトリからAWSへOIDC認証でデプロイを可能にする (#764)
    * カスタムワードを追加します。
    
    * pre-commit.ymlを更新し、terraform関連ファイルを追加しました。
    
    * 更新ラベラーアクションのバージョンをv5に変更
    
    * 更新自動アサインアクションのバージョン。
    
    * .asdfrc に legacy_version_file = yes を追加する
    
    * 新しいTerraformバージョンを追加します。
    
    * ラベラーの設定を更新します。
    
    * Update terraform workflow for AWS management, portfolio, and sandbox. (50 characters)
    
    * Terraformワークフローのブランチ設定を更新
    
    * time_capsule を追加する。
    
    * terraform-docs: automated action
    
    * 不要なジョブ名を削除します。
    
    * アップデートシェルチェック v0.9.0 から v0.10.0 に変更
    
    * ワークフローの成功と失敗を処理するステップを追加
    
    * バージョンを1.6.6から1.7.5に更新する。
    
    * バージョンを1.7.5に更新する。
    
    * tfstateファイルのキーを更新する
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ terraform/environments/portfolio/time_capsule/provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = local.region.apne1
+  default_tags {
+    tags = merge(local.common_tags, {
+      "${local.prefix}:source_path" = "./terraform/environments/${local.aws_env_name}/time_capsule"
+    })
+  }
+}
