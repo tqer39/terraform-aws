96c13547bc81e5fbedd2039da6d7f914971a3ff5
tqer39 <tqer39@gmail.com>
2024-04-14 17:56:56 +0900

    Add time capsule Terraform configuration for sandbox environment

---
+++ terraform/environments/sandbox/time_capsule/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.43.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-107662415716-ap-northeast-1"
+    encrypt = true
+    key     = "terraform-aws/terraform/environments/sandbox/sandbox-time_capsule.tfstate"
+    region  = "ap-northeast-1"
+  }
+}
