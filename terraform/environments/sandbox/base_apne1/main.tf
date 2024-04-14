103aea6a60fe870db1eda4354a387e82d3b3fb0f
tqer39 <tqer39@gmail.com>
2024-04-14 17:55:39 +0900

    oidc_github_actions_id_providerを追加

--- terraform/environments/sandbox/base_apne1/main.tf
+++ terraform/environments/sandbox/base_apne1/main.tf
@@ -4,10 +4,6 @@ module "tfstate_s3_bucket" {
   bucket_name = local.tfstate_bucket_name.apne1
 }
 
-module "oidc_github_actions_id_provider" {
-  source = "../../../usecases/id_provider"
-}
-
 module "deploy_role" {
   source = "../../../usecases/deploy_role/terraform_aws"
 
@@ -19,6 +15,10 @@ module "deploy_role" {
     aws_iam_policy.deploy_allow_specifics,
     aws_iam_policy.deploy_deny_specifics,
   ]
+}
+
+module "oidc_github_actions_id_provider" {
+  source = "../../../usecases/id_provider"
 }
 
 data "aws_iam_policy_document" "deploy_allow_specifics" {
@@ -89,46 +89,4 @@ data "aws_iam_policy_document" "deploy_deny_specifics" {
 resource "aws_iam_policy" "deploy_deny_specifics" {
   name   = "deploy-deny-specifics"
   policy = data.aws_iam_policy_document.deploy_deny_specifics.json
-}
-
-data "aws_iam_policy_document" "allow_policies_time_capsule" {
-  statement {
-    sid = "Amplify"
-    actions = [
-      "amplify:CreateApp",
-      "amplify:TagResource",
-    ]
-    resources = ["arn:aws:amplify:ap-northeast-1:107662415716:apps/*"]
-  }
-  statement {
-    sid = "tfstate"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket",
-    ]
-    resources = [
-      "arn:aws:s3:::terraform-tfstate-tqer39-107662415716-ap-northeast-1",
-      "arn:aws:s3:::terraform-tfstate-tqer39-107662415716-ap-northeast-1/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "allow_policies_time_capsule" {
-  name   = "allow-policies-time-capsule"
-  policy = data.aws_iam_policy_document.allow_policies_time_capsule.json
-}
-
-module "deploy_role_time_capsule" {
-  source = "../../../usecases/deploy_role/time_capsule"
-
-  aws_account_id = local.aws_account_id
-  aws_env_name   = local.aws_env_name
-  organization   = local.organization
-  repository     = local.repository
-  depends_on = [
-    aws_iam_policy.allow_policies_time_capsule,
-    aws_iam_policy.deploy_deny_specifics,
-  ]
 }
