a242d92127716536f4dae687cdec41ae381f9783
Takeru O'oyama <tqer39@gmail.com>
2024-04-14 12:37:18 +0900

    Feature/maintenance 2024 04 10 2331 (#780)
    * Add oidc_github_actions_id_provider module
    
    * タイムカプセルポリシーを許可します。
    
    * auto_assign.ymlを更新してレビュアーを追加しないように変更する。
    
    * ドメイン名を更新しました。
    
    * Update domain source in main.tf to use the correct path
    
    * terraform-docs: automated action
    
    * Add oidc_github_actions_id_provider module
    
    * ドメイン名を更新する。
    
    * 管理ドメインの出力を削除します。
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/environments/management/domains/main.tf
+++ terraform/environments/management/domains/main.tf
@@ -1,20 +1,5 @@
 module "root_domain" {
-  source = "../../../usecases/root_domain"
+  source = "../../../usecases/domain"
 
-  domain_name = "tqer39.com"
-}
-
-resource "aws_route53_record" "portfolio" {
-  zone_id = module.root_domain.zone_id
-  name    = "portfolio.tqer39.com"
-  type    = "NS"
-  ttl     = "30"
-  # TODO: portfolio のアカウントから取得できるようになったらハードコーディングをやめる
-  # records = data.terraform_remote_state.portfolio-domains.outputs.name_servers
-  records = [
-    "ns-1761.awsdns-28.co.uk.",
-    "ns-958.awsdns-55.net.",
-    "ns-500.awsdns-62.com.",
-    "ns-1366.awsdns-42.org.",
-  ]
+  domain_name = "tqer39.dev"
 }
