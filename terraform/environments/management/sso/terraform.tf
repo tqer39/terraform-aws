34946830911ddc27242bf07099d46f65709aa9a5
Takeru O'oyama <tqer39@gmail.com>
2024-04-20 17:26:18 +0900

    Feature/maintenance 2024 04 20 1717 (#807)
    * ワークフローを更新する。
    
    * 管理Ssoディレクトリにファイルをリネームしました

--- terraform/environments/management/sso/terraform.tf
+++ terraform/environments/management/sso/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-tfstate-tqer39-577523824419-ap-northeast-1"
     encrypt = true
-    key     = "terraform-aws/terraform/environments/management/management-single_sign_on.tfstate"
+    key     = "management/management-sso.tfstate"
     region  = "ap-northeast-1"
   }
 }
