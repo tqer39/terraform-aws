3357124d1d29be2e9e3e7cedd09747b7b825cd1d
tqer39 <tqer39@gmail.com>
2024-04-20 17:21:54 +0900

    管理Ssoディレクトリにファイルをリネームしました

--- terraform/environments/management/sso/provider.tf
+++ terraform/environments/management/sso/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = local.region.apne1
   default_tags {
     tags = merge(local.common_tags, {
-      "${local.prefix}:source_path" = "./terraform/environments/${local.aws_env_name}/single_sign_on"
+      "${local.prefix}:source_path" = "./terraform/environments/${local.aws_env_name}/sso"
     })
   }
 }
