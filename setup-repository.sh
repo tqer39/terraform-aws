756f8c31affc74f14eb8a74d5d27d53241c68b27
tqer39 <tqer39@gmail.com>
2025-03-20 17:24:47 +0900

    feat: session-manager-pluginがインストールされていない場合のインストール手順を追加

--- setup-repository.sh
+++ setup-repository.sh
@@ -67,3 +67,14 @@ if ! command -v rancher-desktop &> /dev/null; then
     sudo apt-get install -y rancher-desktop
   fi
 fi
+
+# Install session-manager-plugin
+if ! command -v session-manager-plugin &> /dev/null; then
+  if [ "$(uname)" == 'Darwin' ]; then
+    brew install --cask session-manager-plugin
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "/tmp/session-manager-plugin.deb"
+    sudo dpkg -i /tmp/session-manager-plugin.deb
+    rm /tmp/session-manager-plugin.deb
+  fi
+fi
